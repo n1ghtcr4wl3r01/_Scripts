836f1ccf16c590fbf59a3783eefc8a4a860101b5
n1ghtcr4wl3r01 <ballo.gohonas@gmail.com>
2017-02-19 18:12:50 +1100

    Unity - Space Shooter Scripts
    All scripts used in the Unity Tutorial "Space Shooter"

---
+++ GameController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValue;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+
+        score = 0;
+        UpdateScore();
+        StartCoroutine(Spawnwaves());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene()
+                .buildIndex);
+            }
+        }
+    }
+
+    IEnumerator Spawnwaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while(true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' to Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+
+    public void AddScore (int newScoreValue)
+    {
+        score = newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score " + score;
+    }
+    
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over, Man";
+        gameOver = true;
+    }
+}
